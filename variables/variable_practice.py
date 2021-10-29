141beb406238e7a8a71c6d4d7936a10c02b79f35
Tyronesha <90337062+Tyronesha@users.noreply.github.com>
2021-10-29 13:01:49 -0400

    Update variable_practice.py

--- variables/variable_practice.py
+++ variables/variable_practice.py
@@ -1,1 +1,9 @@
-# Variables Practice, Tyronesha Alexander, 10/29/2021 12:50pm, version 0.0
+# Variables Practice, Tyronesha Alexander, 10/29/2021 1:01pm, version 0.1
+
+
+# Basic Variables
+
+my_int = -15 # INTERGERS, any +/- whole number and 0
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal
+my_string = "How much wood could a wood chuck chuck if a wood chuck could chuck wood?"
+
